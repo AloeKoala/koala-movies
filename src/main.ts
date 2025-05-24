12899d9b370268ed46af9d48eb9e73d4109cbbeb
xthirtysix <ggalliosha@gmail.com>
2025-05-24 23:54:01 +0300

    🏗️ build: update vite

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import ui from '@nuxt/ui/vue-plugin'
-import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router'
+import router from './router'
 
 const app = createApp(App)
-
-// Define your routes
-const routes: RouteRecordRaw[] = [
-  { path: '/', redirect: '/' },
-  { path: '/:catchAll(.*)', redirect: '/' }
-];
-
-// Create the router instance with createWebHashHistory
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-});
 
 app.use(router)
 app.use(ui)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ui from '@nuxt/ui/vite'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
