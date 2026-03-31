e0f836818dd13cadc2441ab16ccc00ec1c95698f
xthirtysix <ggalliosha@gmail.com>
2026-03-31 08:07:48 +0300

    ✨ feat: update to v1.2

--- src/widgets/statistics/ui/top-users.vue
+++ src/widgets/statistics/ui/top-users.vue
@@ -6,24 +6,24 @@ defineProps<{ list: User[]; title: string }>()
 
 <template>
   <u-card>
-    <div class="flex gap-3 py-2 w-full col-span-full mb-4 justify-center">
-      <u-icon name="i-lucide-users" class="text-green-500 size-7 mb-2" />
+    <div class="flex gap-3 py-2 w-full col-span-full mb-4 justify-center items-center">
+      <u-icon name="i-ph:crown-simple" class="text-yellow-500 size-9" />
       <span
         class="font-amatic text-3xl font-bold text-center max-w-min overflow-hidden text-ellipsis truncate whitespace-nowrap w-full"
       >
         {{ title }}
       </span>
     </div>
-    <ul class="grid grid-cols-subgrid col-span-full gap-4">
+    <ul class="grid grid-cols-subgrid col-span-full gap-4 p-2">
       <li
         v-for="user in list"
         :key="user.nick"
-        class="flex items-center justify-between p-2 rounded-xl ring-1 ring-default"
+        class="flex items-center justify-between p-2 rounded-lg ring-1 ring-default"
       >
         <span class="truncate w-full max-w-min whitespace-nowrap overflow-hidden text-ellipsis">
           {{ user.nick }}
         </span>
-        <u-badge color="primary" variant="outline" class="ml-2">{{ user.count }}</u-badge>
+        <u-badge color="primary" variant="subtle" class="ml-2">{{ user.count }}</u-badge>
       </li>
     </ul>
   </u-card>
